5538f5803f34bfd4f5e37cf53db94cb204fb845b
Rodolfo Felipe <rodolforfq@gmail.com>
2023-01-03 00:02:28 -0500

    - Fixed compatibility issues with V14. - Refactored PolymerTemplate to Component. - General improvements. - Updated version numbers throughout all project. Synced version numbers.

--- autocomplete/src/main/java/com/vaadin/componentfactory/Autocomplete.java
+++ autocomplete/src/main/java/com/vaadin/componentfactory/Autocomplete.java
@@ -1,17 +1,34 @@
 package com.vaadin.componentfactory;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import com.vaadin.componentfactory.Autocomplete.AutocompleteValueAppliedEvent;
-import com.vaadin.flow.component.*;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.DomEvent;
+import com.vaadin.flow.component.EventData;
+import com.vaadin.flow.component.Focusable;
+import com.vaadin.flow.component.HasSize;
+import com.vaadin.flow.component.HasTheme;
+import com.vaadin.flow.component.HasValidation;
+import com.vaadin.flow.component.HasValue;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.KeyModifier;
+import com.vaadin.flow.component.ShortcutRegistration;
+import com.vaadin.flow.component.Synchronize;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.polymertemplate.EventHandler;
-import com.vaadin.flow.component.polymertemplate.Id;
-import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.shared.Registration;
-import com.vaadin.flow.templatemodel.TemplateModel;
 
-import java.util.List;
+import elemental.json.JsonArray;
+import elemental.json.JsonFactory;
+import elemental.json.impl.JreJsonFactory;
 
 /*
  * #%L
@@ -44,9 +61,8 @@ import java.util.List;
 @Tag("vcf-autocomplete")
 @NpmPackage(value = "@vaadin-component-factory/vcf-autocomplete", version = "1.2.10")
 @JsModule("@vaadin-component-factory/vcf-autocomplete/src/vcf-autocomplete.js")
-public class Autocomplete extends
-        PolymerTemplate<Autocomplete.AutocompleteTemplateModel> implements
-        HasTheme, HasSize, HasValue<AutocompleteValueAppliedEvent, String>,
+public class Autocomplete extends Component implements HasTheme, HasSize,
+        HasValue<AutocompleteValueAppliedEvent, String>,
         Focusable<Autocomplete>, HasValidation {
 
     // PROPERTIES
@@ -56,22 +72,31 @@ public class Autocomplete extends
     private static final String LABEL_PROP = "label";
     private static final String PLACEHOLDER_PROP = "placeholder";
     private static final String CASESENSITIVE_PROP = "caseSensitive";
+    private static final String OPTIONS = "options";
 
-    @Id
-    private TextField textField;
+    // Component state attributes
+    private boolean readOnly;
+    private boolean requiredIndicatorVisible;
+    private boolean invalid;
+    private int tabIndex;
+    private String errorMessage;
+
+    // ShadowRoot selector
+    private static final String TEXTFIELD_SELECTOR = "this.$.textField";
 
     /**
      * Default constructor.
      */
     public Autocomplete() {
-        textField.setSizeFull();
+        getElement().executeJs(String.format("textFieldStyles = %s.style;%s%s",
+                TEXTFIELD_SELECTOR, "textFieldStyles.width='100%';",
+                "textFieldStyles.height='100%';"));
     }
 
     /**
      * Constructor that sets the maximum number of displayed options.
      *
-     * @param limit
-     *            maximum number of displayed options
+     * @param limit maximum number of displayed options
      */
     public Autocomplete(int limit) {
         this();
@@ -94,11 +119,15 @@ public class Autocomplete extends
      * <p>
      * The maximum displayed options will be lower-equal to the set limit.
      *
-     * @param options
-     *            Hints/options to the user
+     * @param options Hints/options to the user
      */
     public void setOptions(List<String> options) {
-        getModel().setOptions(options);
+        JsonFactory jsonFactory = new JreJsonFactory();
+        JsonArray jsonArray = jsonFactory.createArray();
+        for (int i = 0; i < options.size(); i++) {
+            jsonArray.set(i, options.get(i));
+        }
+        getElement().setPropertyJson(OPTIONS, jsonArray);
     }
 
     /**
@@ -107,7 +136,12 @@ public class Autocomplete extends
      * @return options list of options/hints/suggestions
      */
     public List<String> getOptions() {
-        return getModel().getOptions();
+        List<String> optionsList = new ArrayList<>();
+        JsonArray optionsArray = (JsonArray) getElement().getPropertyRaw(OPTIONS);
+        for (int i = 0; i < optionsArray.length(); i++) {
+            optionsList.add(optionsArray.getString(i));
+        }
+        return optionsList;
     }
 
     /**
@@ -115,8 +149,7 @@ public class Autocomplete extends
      * <p>
      * The limit should be bigger than 0.
      *
-     * @param limit
-     *            maximum number of displayed options
+     * @param limit maximum number of displayed options
      */
     public void setLimit(int limit) {
         getElement().setProperty(LIMIT_PROP, limit);
@@ -126,7 +159,7 @@ public class Autocomplete extends
      * Gets the the maximum number of displayed options.
      *
      * @return limit maximum number of displayed options. -1 is returned if
-     *         there is not a limit set.
+     * there is not a limit set.
      */
     public int getLimit() {
         return getElement().getProperty(LIMIT_PROP, -1);
@@ -135,8 +168,7 @@ public class Autocomplete extends
     /**
      * Sets the highlight to be case sensitive or not
      *
-     * @param caseSensitive
-     *            true = case sensitive highlighting
+     * @param caseSensitive true = case sensitive highlighting
      */
     public void setCaseSensitive(boolean caseSensitive) {
         getElement().setProperty(CASESENSITIVE_PROP, caseSensitive);
@@ -156,9 +188,8 @@ public class Autocomplete extends
      * <p>
      * It is useful if the loading of data is slow.
      *
-     * @param loading
-     *            <code>true</code> show loading icon <code>false</code>,
-     *            otherwise
+     * @param loading <code>true</code> show loading icon <code>false</code>,
+     *                otherwise
      */
     public void setLoading(boolean loading) {
         getElement().setProperty(LOADING_PROP, loading);
@@ -168,7 +199,7 @@ public class Autocomplete extends
      * Checks if there is loading icon in the Autocomplete panel.
      *
      * @return loading <code>true</code> loading icon is visible
-     *         <code>false</code>, otherwise
+     * <code>false</code>, otherwise
      */
     public boolean isLoading() {
         return getElement().getProperty(LOADING_PROP, false);
@@ -195,8 +226,7 @@ public class Autocomplete extends
      * <p>
      * A placeholder string in addition to the label.
      *
-     * @param placeholder
-     *            the String value to set
+     * @param placeholder the String value to set
      */
     public void setPlaceholder(String placeholder) {
         getElement().setProperty(PLACEHOLDER_PROP,
@@ -222,8 +252,7 @@ public class Autocomplete extends
      * <p>
      * String used for the label element.
      *
-     * @param label
-     *            the String value to set
+     * @param label the String value to set
      */
     public void setLabel(String label) {
         getElement().setProperty(LABEL_PROP, label == null ? "" : label);
@@ -233,8 +262,7 @@ public class Autocomplete extends
      * Adds a listener for {@code ValueChangeEvent} events fired by the
      * webcomponent.
      *
-     * @param listener
-     *            the listener
+     * @param listener the listener
      * @return a {@link Registration} for removing the event listener
      */
     public Registration addChangeListener(
@@ -246,8 +274,7 @@ public class Autocomplete extends
      * Adds a listener for {@code AutocompleteValueAppliedEvent} events fired by
      * the webcomponent.
      *
-     * @param listener
-     *            the listener
+     * @param listener the listener
      * @return a {@link Registration} for removing the event listener
      */
     public Registration addAutocompleteValueAppliedListener(
@@ -258,8 +285,7 @@ public class Autocomplete extends
     /**
      * Adds a listener for {@code ValueClearEvent}.
      *
-     * @param listener
-     *            the listener
+     * @param listener the listener
      * @return a {@link Registration} for removing the event listener
      */
     public Registration addValueClearListener(
@@ -282,7 +308,7 @@ public class Autocomplete extends
         private final String value;
 
         public AucompleteChangeEvent(Autocomplete source, boolean fromClient,
-                @EventData("event.detail.value") String value) {
+                                     @EventData("event.detail.value") String value) {
             super(source, fromClient);
             this.value = value;
         }
@@ -304,8 +330,8 @@ public class Autocomplete extends
         private final String value;
 
         public AutocompleteValueAppliedEvent(Autocomplete source,
-                boolean fromClient,
-                @EventData("event.detail.value") String value) {
+                                             boolean fromClient,
+                                             @EventData("event.detail.value") String value) {
             super(source, fromClient);
             this.value = value;
             this.source = source;
@@ -340,114 +366,88 @@ public class Autocomplete extends
         }
     }
 
-    /**
-     * This model binds properties {@link Autocomplete} and
-     * vcf-autocomplete.html
-     */
-    public interface AutocompleteTemplateModel extends TemplateModel {
-        List<String> getOptions();
-
-        void setOptions(List<String> options);
-    }
-
     public void setValue(String value) {
         getElement().executeJs("this._applyValue(\"" + value + "\");");
     }
 
     @Override
     public void setReadOnly(boolean readOnly) {
-        if (readOnly) {
-            getElement().getStyle().set("pointer-events", "none");
-        } else {
-            getElement().getStyle().set("pointer-events", "auto");
-        }
-        textField.setReadOnly(readOnly);
+        this.readOnly = readOnly;
+        getElement().getStyle().set("pointer-events", readOnly ? "none" : "auto");
+        getElement().executeJs(String.format("%s.readonly=$0", TEXTFIELD_SELECTOR), readOnly);
     }
 
     @Override
     public boolean isReadOnly() {
-        return textField.isReadOnly();
+        return this.readOnly;
     }
 
     @Override
     public void setRequiredIndicatorVisible(boolean requiredIndicatorVisible) {
-        textField.setRequiredIndicatorVisible(requiredIndicatorVisible);
+        this.requiredIndicatorVisible = requiredIndicatorVisible;
+        getElement().executeJs(String.format("%s.required=$0", TEXTFIELD_SELECTOR), requiredIndicatorVisible);
     }
 
     @Override
     public boolean isRequiredIndicatorVisible() {
-        return textField.isRequiredIndicatorVisible();
+        return this.requiredIndicatorVisible;
     }
 
     @Override
     public Registration addValueChangeListener(
             ValueChangeListener<? super AutocompleteValueAppliedEvent> listener) {
-        return addAutocompleteValueAppliedListener(event -> {
-            listener.valueChanged(event);
-        });
+        return addAutocompleteValueAppliedListener(listener::valueChanged);
     }
 
     @Override
     public String getErrorMessage() {
-        if (textField != null) {
-            return textField.getErrorMessage();
-        } else {
-            return null;
-        }
+        return Objects.nonNull(this.errorMessage) ? this.errorMessage : "";
     }
 
     @Override
     public boolean isInvalid() {
-        if (textField != null) {
-            return textField.isInvalid();
-        } else {
-            return false;
-        }
+        return this.invalid;
     }
 
     @Override
     public void setErrorMessage(String errorMessage) {
-        if (textField != null) {
-            textField.setErrorMessage(errorMessage);
-        }
+        this.errorMessage = errorMessage;
+        getElement().executeJs(String.format("%s.errorMessage=$0", TEXTFIELD_SELECTOR), errorMessage);
     }
 
     @Override
     public void setInvalid(boolean invalid) {
-        if (textField != null) {
-            textField.setInvalid(invalid);
-        }
+        this.invalid = invalid;
+        getElement().executeJs(String.format("%s.invalid=$0", TEXTFIELD_SELECTOR), invalid);
     }
 
     @Override
     public void focus() {
-        if (textField != null) {
-            textField.focus();
-        }
+        getElement().executeJs(String.format("%s.focus();", TEXTFIELD_SELECTOR));
     }
 
     @Override
     public void blur() {
-        if (textField != null) {
-            textField.blur();
-        }
+        getElement().executeJs(String.format("%s.blur();", TEXTFIELD_SELECTOR));
     }
 
     @Override
     public void setTabIndex(int tabIndex) {
-        if (textField != null) {
-            textField.setTabIndex(tabIndex);
-        }
+        this.tabIndex = tabIndex;
+        getElement().executeJs(String.format("%s.tabindex=$0", TEXTFIELD_SELECTOR), tabIndex);
     }
 
     @Override
     public int getTabIndex() {
-        return textField.getTabIndex();
+        return this.tabIndex;
     }
 
     @Override
     public ShortcutRegistration addFocusShortcut(Key key,
-            KeyModifier... keyModifiers) {
-        return textField.addFocusShortcut(key, keyModifiers);
+                                                 KeyModifier... keyModifiers) {
+        return UI.getCurrent().addShortcutListener(
+                () -> getElement().executeJs(String.format("%s.focus();",
+                        TEXTFIELD_SELECTOR)), key, keyModifiers);
     }
+
 }
